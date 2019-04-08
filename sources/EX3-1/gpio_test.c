5ea0e4379f7b365c7208e3d8c084a38950abe29e
Polo Woo <polo@inipro.net>
2019-04-08 18:34:17 +0900

    2019.1

--- sources/EX3-1/gpio_test.c
+++ sources/EX3-1/gpio_test.c
@@ -9,17 +9,17 @@ int main (void)
 
 	print("-- Start of the Program --\r\n");
 
-	XGpio_Initialize(&leds, XPAR_AXI_GPIO_0_DEVICE_ID);
-	XGpio_SetDataDirection(&leds, 1, 0x00000000);
-
-	XGpio_Initialize(&btns, XPAR_AXI_GPIO_1_DEVICE_ID);
+	XGpio_Initialize(&btns, XPAR_AXI_GPIO_0_DEVICE_ID);
 	XGpio_SetDataDirection(&btns, 1, 0xffffffff);
+
+	XGpio_Initialize(&leds, XPAR_AXI_GPIO_0_DEVICE_ID);
+	XGpio_SetDataDirection(&leds, 2, 0x00000000);
 
 	while (1)
 	{
 		btns_check = XGpio_DiscreteRead(&btns, 1);
-		XGpio_DiscreteWrite(&leds, 1, btns_check);
-		xil_printf("Switch Status : %x\r\n", btns_check);
+		XGpio_DiscreteWrite(&leds, 2, btns_check);
+		xil_printf("Button Status : %x\r\n", btns_check);
 		sleep(1);
 	}
 }
